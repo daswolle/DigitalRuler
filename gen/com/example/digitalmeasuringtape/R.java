1d9ee9b66792cd72061984217273b0f205326510
daswolle <dragohat@gmail.com>
2013-02-25 20:59:12 -0500

    updated button look

--- gen/com/example/digitalmeasuringtape/R.java
+++ gen/com/example/digitalmeasuringtape/R.java
@@ -21,7 +21,14 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int btn_default_normal=0x7f020000;
+        public static final int btn_default_normal_disable=0x7f020001;
+        public static final int btn_default_normal_disable_focused=0x7f020002;
+        public static final int btn_default_normal_green=0x7f020003;
+        public static final int btn_default_pressed=0x7f020004;
+        public static final int btn_default_selected=0x7f020005;
+        public static final int custom_button_green=0x7f020006;
+        public static final int ic_launcher=0x7f020007;
     }
     public static final class id {
         public static final int action_settings=0x7f080005;
--- src/com/example/digitalmeasuringtape/MainActivity.java
+++ src/com/example/digitalmeasuringtape/MainActivity.java
@@ -5,7 +5,7 @@ import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
-import android.view.KeyEvent;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.TextView;
 
@@ -14,6 +14,7 @@ public class MainActivity extends Activity implements Runnable{
 	private String pi_string;
 	private TextView tv;
 	private ProgressDialog pd;
+	private boolean activeThread = true;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -22,22 +23,37 @@ public class MainActivity extends Activity implements Runnable{
 		
 		tv = (TextView) this.findViewById(R.id.text);
 		tv.setText("--");
+
 		
 	}
 	
 	//connected to button's onClick
 	public void record_measurements(View view){
-		pd = ProgressDialog.show(this, "Working..", "Calculating Pi", true, false);
+		//false below is for cancleable; may need to change
+		pd = ProgressDialog.show(this, "Working..", "Calculating", true, false);
 		Thread thread = new Thread(this);
 		thread.start();
 	}
 	
 	//put the code to be run during execution here
 	public void run(){
-		pi_string = (Math.floor((Math.random()*5)+2))+ "\"";
-		//signal the outside world
-		handler.sendEmptyMessage(0);
+		while (activeThread){
+			pi_string = (Math.floor((Math.random()*5)+2))+ "\"";
+			//signal the outside world
+			handler.sendEmptyMessage(0);
+		}
 	}
+	
+	// manages user touching the screen
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+            // we set the activeThread boolean to false,
+            // forcing the loop from the Thread to end
+            activeThread = false;
+        }
+        return super.onTouchEvent(event);
+    }
 	
 	//Receive thread messages, interpret them and act as needed
 	private Handler handler = new Handler(){
