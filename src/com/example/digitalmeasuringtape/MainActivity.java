50bbf358e762c2aac7341fea17f4fc321ecff0ce
Peter Beissinger <pab60@case.edu>
2013-02-26 21:26:23 -0500

    Added distance calculation function call, "Distance"

--- src/com/example/digitalmeasuringtape/MainActivity.java
+++ src/com/example/digitalmeasuringtape/MainActivity.java
@@ -64,5 +64,119 @@ public class MainActivity extends Activity implements Runnable{
 		}
 	};
 	
+	public static double Distance(	ArrayList<Double> x_accel, 
+									ArrayList<Double> y_accel, 
+									ArrayList<Double> z_accel,
+									ArrayList<Double> t)
+	{
+		//This is the Euclid's method.
+		ArrayList<Double> dx_veloc = new ArrayList<Double>(); dx_veloc.add(0.0);
+		ArrayList<Double> dy_veloc = new ArrayList<Double>(); dy_veloc.add(0.0);
+		ArrayList<Double> dz_veloc = new ArrayList<Double>(); dz_veloc.add(0.0);
+		
+		ArrayList<Double> x_veloc = new ArrayList<Double>(); x_veloc.add(0.0);
+		ArrayList<Double> y_veloc = new ArrayList<Double>(); y_veloc.add(0.0);
+		ArrayList<Double> z_veloc = new ArrayList<Double>(); z_veloc.add(0.0);
+		
+		//compose velocity
+		int I = x_accel.size();
+		double dt;
+		System.out.println("Composing Velocity...\n");
+		for( int i = 1; i < I; i++ )
+		{	
+			//x'_i = x''_(i-1) * dt
+			//y'_i = y''_(i-1) * dt
+			//z'_i = z''_(i-1) * dt
+			dt = t.get(i) - t.get(i-1);
+			dx_veloc.add(  x_accel.get(i-1) * dt);
+			dy_veloc.add(  y_accel.get(i-1) * dt);
+			dz_veloc.add(  z_accel.get(i-1) * dt);
+			System.out.println("Step: " + i + "\n\tv_x: "+ dx_veloc.get(i) + "\n\tv_y: " + dy_veloc.get(i) + "\n\tv_z: " + dz_veloc.get(i));
+		}
+		double temp = 0;
+		for(double d : dx_veloc)
+			{
+				temp += d;
+				x_veloc.add(temp);
+			}
+		
+		temp = 0;
+		for(double d : dy_veloc)
+			{
+				temp += d;
+				y_veloc.add(temp);
+			}
+		
+		temp = 0;
+		for(double d : dz_veloc)
+			{
+				temp += d;
+				z_veloc.add(temp);
+			}
+		
+		ArrayList<Double> dx_disp = new ArrayList<Double>(); dx_disp.add(0.0); dx_disp.add(0.0);
+		ArrayList<Double> dy_disp = new ArrayList<Double>(); dy_disp.add(0.0); dy_disp.add(0.0);
+		ArrayList<Double> dz_disp = new ArrayList<Double>(); dz_disp.add(0.0); dz_disp.add(0.0);
+		
+		ArrayList<Double> x_disp = new ArrayList<Double>(); x_disp.add(0.0); x_disp.add(0.0);
+		ArrayList<Double> y_disp = new ArrayList<Double>(); y_disp.add(0.0); y_disp.add(0.0);
+		ArrayList<Double> z_disp = new ArrayList<Double>(); z_disp.add(0.0); z_disp.add(0.0);
+		
+		//compose displacement
+		I = t.size();
+		for( int i = 2; i < I; i++ )
+		{	
+			//x_i = x'_(i-1) * dt
+			//y_i = y'_(i-1) * dt
+			//z_i = z'_(i-1) * dt
+			dt = t.get(i) - t.get(i-1);
+			dx_disp.add( x_veloc.get(i-1) * dt);
+			dy_disp.add( y_veloc.get(i-1) * dt);
+			dz_disp.add( z_veloc.get(i-1) * dt);
+		}
+		
+		//compose total displacement
+		double distance = 0;
+
+		if( true/*Euclidean_Distance_Mode */)
+		{
+			//vector addition, constructing R
+			System.out.println("Composing R...\n");
+			double r[] = new double[3]; //[x, y, z]
+			for( int i = 0; i < I; i++)
+			{
+				r[0] += dx_disp.get(i);
+				r[1] += dy_disp.get(i);
+				r[2] += dz_disp.get(i);
+				System.out.println("Step: " + i + "\n\tr_x: "+ r[0] + "\n\tr_y: " + r[1] + "\n\tr_z: " + r[2]);
+			}
+		
+			//Distance formula, constructing D
+			//D = sqrt(X^2 + Y^2 + Z^2)
+			distance = Math.sqrt( 
+							Math.pow(r[0], 2) + 
+							Math.pow(r[1], 2) +
+							Math.pow(r[2], 2)
+							);
+			return distance;
+		}
+
+		else if ( false /*Path_Distance_Mode */)
+		{
+			//sum up individual distances, constructing D
+			for( int i = 0; i < I; i++)
+			{
+				//dD = sqrt( dx^2 + dy^2 + dz^2 )
+				distance += Math.sqrt(
+								Math.pow(dx_disp.get(i), 2) +
+								Math.pow(dy_disp.get(i), 2) +
+								Math.pow(dz_disp.get(i), 2)
+								);
+			}		
+			return distance;
+		}  
+	return 0; //won't get here.
+}
+	
 
 }
