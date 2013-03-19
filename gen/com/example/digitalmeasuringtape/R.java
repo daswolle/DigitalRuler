2bffabffc985644f7fc92c08fdce76ecd0d71ec0
Peter Beissinger <pab60@case.edu>
2013-03-19 01:17:28 -0400

    First commit to branch-1

--- gen/com/example/digitalmeasuringtape/R.java
+++ gen/com/example/digitalmeasuringtape/R.java
@@ -31,29 +31,37 @@ public final class R {
         public static final int ic_launcher=0x7f020007;
     }
     public static final class id {
-        public static final int action_settings=0x7f080005;
-        public static final int button1=0x7f080004;
-        public static final int radio0=0x7f080001;
-        public static final int radio1=0x7f080002;
-        public static final int radioGroup1=0x7f080000;
-        public static final int text1=0x7f080003;
+        public static final int about_content=0x7f080000;
+        public static final int about_menuitem=0x7f080008;
+        public static final int action_settings=0x7f080006;
+        public static final int button1=0x7f080005;
+        public static final int getMinDelay=0x7f080009;
+        public static final int help=0x7f080007;
+        public static final int radio0=0x7f080002;
+        public static final int radio1=0x7f080003;
+        public static final int radioGroup1=0x7f080001;
+        public static final int text1=0x7f080004;
     }
     public static final class layout {
-        public static final int activity_display_message=0x7f030000;
-        public static final int activity_main=0x7f030001;
+        public static final int about=0x7f030000;
+        public static final int activity_display_message=0x7f030001;
+        public static final int activity_main=0x7f030002;
     }
     public static final class menu {
         public static final int display_message=0x7f070000;
         public static final int main=0x7f070001;
     }
     public static final class string {
+        public static final int about=0x7f050008;
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
-        public static final int button_send=0x7f050006;
-        public static final int hello_world=0x7f050002;
-        public static final int measurement=0x7f050005;
-        public static final int mode0=0x7f050003;
-        public static final int mode1=0x7f050004;
+        public static final int button_send=0x7f050007;
+        public static final int getMinDelay=0x7f050009;
+        public static final int hello_world=0x7f050003;
+        public static final int help=0x7f050002;
+        public static final int measurement=0x7f050006;
+        public static final int mode0=0x7f050004;
+        public static final int mode1=0x7f050005;
     }
     public static final class style {
         /** 
