57d34df98451ec6f00b279c86fbae082571c0c66
Peter Beissinger <pab60@case.edu>
2013-03-19 00:51:15 -0400

    Revert "added R thing and updates"
    This reverts commit d83d8e6cdf6e655c10e2ae93ffe7d159d11e758c.

--- gen/com/example/digitalmeasuringtape/R.java
+++ gen/com/example/digitalmeasuringtape/R.java
@@ -8,10 +8,6 @@
 package com.example.digitalmeasuringtape;
 
 public final class R {
-    public static final class array {
-        public static final int settings_list=0x7f040000;
-        public static final int settings_list_values=0x7f040001;
-    }
     public static final class attr {
     }
     public static final class dimen {
@@ -21,8 +17,8 @@ public final class R {
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f050000;
-        public static final int activity_vertical_margin=0x7f050001;
+        public static final int activity_horizontal_margin=0x7f040000;
+        public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
         public static final int btn_default_normal=0x7f020000;
@@ -35,36 +31,29 @@ public final class R {
         public static final int ic_launcher=0x7f020007;
     }
     public static final class id {
-        public static final int about_content=0x7f090000;
-        public static final int action_settings=0x7f090003;
-        public static final int button1=0x7f090002;
-        public static final int help=0x7f090005;
-        public static final int settings_menuitem=0x7f090004;
-        public static final int text1=0x7f090001;
+        public static final int action_settings=0x7f080005;
+        public static final int button1=0x7f080004;
+        public static final int radio0=0x7f080001;
+        public static final int radio1=0x7f080002;
+        public static final int radioGroup1=0x7f080000;
+        public static final int text1=0x7f080003;
     }
     public static final class layout {
-        public static final int about=0x7f030000;
-        public static final int activity_display_message=0x7f030001;
-        public static final int activity_main=0x7f030002;
-        public static final int settings=0x7f030003;
+        public static final int activity_display_message=0x7f030000;
+        public static final int activity_main=0x7f030001;
     }
     public static final class menu {
-        public static final int display_message=0x7f080000;
-        public static final int main=0x7f080001;
+        public static final int display_message=0x7f070000;
+        public static final int main=0x7f070001;
     }
     public static final class string {
-        public static final int about=0x7f060008;
-        public static final int app_name=0x7f060000;
-        public static final int button_send=0x7f060007;
-        public static final int hello_world=0x7f060003;
-        public static final int help=0x7f060002;
-        public static final int measurement=0x7f060006;
-        public static final int mode0=0x7f060004;
-        public static final int mode1=0x7f060005;
-        /**  settings 
-         */
-        public static final int setting_header1=0x7f060009;
-        public static final int settings=0x7f060001;
+        public static final int action_settings=0x7f050001;
+        public static final int app_name=0x7f050000;
+        public static final int button_send=0x7f050006;
+        public static final int hello_world=0x7f050002;
+        public static final int measurement=0x7f050005;
+        public static final int mode0=0x7f050003;
+        public static final int mode1=0x7f050004;
     }
     public static final class style {
         /** 
@@ -88,10 +77,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f070000;
+        public static final int AppBaseTheme=0x7f060000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f070001;
+        public static final int AppTheme=0x7f060001;
     }
 }
