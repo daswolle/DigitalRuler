f5c77983272f60f867c04493e1472b11832a27ff
daswolle <dragohat@gmail.com>
2013-03-23 20:18:50 -0400

    pushing finiky R.java

--- gen/com/example/digitalmeasuringtape/R.java
+++ gen/com/example/digitalmeasuringtape/R.java
@@ -35,17 +35,16 @@ public final class R {
         public static final int ic_launcher=0x7f020007;
     }
     public static final class id {
-        public static final int about_content=0x7f090000;
+        public static final int about_content=0x7f090002;
         public static final int action_settings=0x7f090003;
-        public static final int button1=0x7f090002;
-        public static final int help=0x7f090005;
+        public static final int button1=0x7f090001;
         public static final int settings_menuitem=0x7f090004;
-        public static final int text1=0x7f090001;
+        public static final int text1=0x7f090000;
     }
     public static final class layout {
-        public static final int about=0x7f030000;
-        public static final int activity_display_message=0x7f030001;
-        public static final int activity_main=0x7f030002;
+        public static final int activity_display_message=0x7f030000;
+        public static final int activity_main=0x7f030001;
+        public static final int calibrate=0x7f030002;
         public static final int settings=0x7f030003;
     }
     public static final class menu {
