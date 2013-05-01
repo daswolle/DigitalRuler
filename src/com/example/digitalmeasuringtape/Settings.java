e32d85c4910d658683e3533410ad8bb0b506785d
daswolle <dragohat@gmail.com>
2013-05-01 01:49:27 -0400

    cleaning settings

--- src/com/example/digitalmeasuringtape/Settings.java
+++ src/com/example/digitalmeasuringtape/Settings.java
@@ -7,26 +7,7 @@ public class Settings extends PreferenceActivity{
 	@Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //deprecated, but we're developing for older stuff anyways... can be updated in the future
+        //deprecated, but we're developing for older stuff anyways...
         addPreferencesFromResource(R.layout.settings);
     }
-/*	//can't call method from xml file
-	void calibrate_sensor(){
-    	AlertDialog.Builder builder = new AlertDialog.Builder(this);
-		builder.setPositiveButton("FINIZH", new DialogInterface.OnClickListener(){
-			public void onClick(DialogInterface dialog, int id){
-				//TODO when user clicks	
-			}
-		});
-		builder.setMessage("WERKING").setTitle("TWERKING");
-		AlertDialog dialog = builder.create();
-		WindowManager.LayoutParams wmlp = dialog.getWindow().getAttributes();
-		
-		wmlp.gravity = Gravity.TOP | Gravity.LEFT;
-		wmlp.y = 400;
-		
-		dialog.show();
-		return;
-	}
-	*/
 }
